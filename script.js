ca832b68241f7fb4364f70fcf290f028cbf2a535
klezi10 <74952593+klezi10@users.noreply.github.com>
2022-01-11 18:18:21 +0700

    set up all the buttons in one function

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const billValue = document.getElementById('bill-value');
+const buttons = document.querySelectorAll('.tip-btns');
 
 const fivePercent = document.getElementById('five-percent');
 const tenPercent = document.getElementById('ten-percent');
@@ -9,14 +10,14 @@ const fifty = document.getElementById('fifty-percent');
 const dollarTip = document.querySelector('.dollar-tip');
 const dollarTotal = document.querySelector('.dollar-total');
 
-fivePercent.addEventListener('click', calculateFive);
+for (const button of buttons) {
+  button.addEventListener('click', calculate);
+}
 
-function calculateFive(event) {
-  event.preventDefault();
-  let tip = (billValue.value * parseInt(fivePercent.value)) / 100;
+function calculate(event) {
+  console.log(event.target.defaultValue);
+  let tip = (billValue.value * parseInt(event.target.defaultValue)) / 100;
   let totalBill = tip + parseInt(billValue.value);
-
-  console.log(fivePercent.value);
   console.log(tip);
   console.log(totalBill);
   dollarTip.textContent = `$${parseFloat(tip).toFixed(2)}`;
