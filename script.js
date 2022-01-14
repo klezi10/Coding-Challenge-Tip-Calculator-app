4ae2b6a0bcb74d4d7175e6b6ea9dfaa1f407e147
klezi10 <74952593+klezi10@users.noreply.github.com>
2022-01-14 17:12:27 +0700

    set up tip and bill per person

--- script.js
+++ script.js
@@ -4,12 +4,7 @@ const resetBtn = document.getElementById('reset-btn');
 const dollarTip = document.querySelector('.dollar-tip');
 const dollarTotal = document.querySelector('.dollar-total');
 const customTip = document.getElementById('custom');
-
-// const fivePercent = document.getElementById('five-percent');
-// const tenPercent = document.getElementById('ten-percent');
-// const fifteenPercent = document.getElementById('fifteen-percent');
-// const twentyFive = document.getElementById('twenty-five-percent');
-// const fifty = document.getElementById('fifty-percent');
+const numberOfPeople = document.getElementById('people-value');
 
 // ================TIP BUTTONS =====================
 
@@ -19,11 +14,24 @@ for (const button of buttons) {
 
 function calculate(event) {
   // console.log(event.target.value);
+  event.preventDefault();
   let tip = (billValue.value * parseInt(event.target.value)) / 100;
   let totalBill = tip + parseInt(billValue.value);
 
   dollarTip.textContent = `$${parseFloat(tip).toFixed(2)}`;
   dollarTotal.textContent = `$${parseFloat(totalBill).toFixed(2)}`;
+
+  splitCheck(tip, totalBill);
+}
+
+function splitCheck(tip, totalBill) {
+  numberOfPeople.addEventListener('keyup', (event) => {
+    event.preventDefault();
+    let newTip = tip / numberOfPeople.value;
+    let newTotalBill = totalBill / numberOfPeople.value;
+    dollarTip.textContent = `$${parseFloat(newTip).toFixed(2)}`;
+    dollarTotal.textContent = `$${parseFloat(newTotalBill).toFixed(2)}`;
+  });
 }
 
 // ================= CUSTOM TIP ==============
@@ -46,4 +54,5 @@ resetBtn.addEventListener('click', () => {
   dollarTip.textContent = '$0.00';
   dollarTotal.textContent = '$0.00';
   resetBtn.disabled = true;
+  numberOfPeople.value = '';
 });
